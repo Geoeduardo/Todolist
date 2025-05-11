1b0642b342c7f4c316d079927cfd5d2b7233efc2
Geoeduardo <jedu99@outlook.com>
2025-05-10 19:48:16 -0500

    addTask function added

--- todo_list.js
+++ todo_list.js
@@ -3,3 +3,12 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
 let tasks = [];
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
